b1f49074c3e141648992b135bf366456753e26a4
alex-blair <alex.n.blair@gmail.com>
2017-04-12 22:21:10 +1200

    added auth0

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutPage = () =>
+  <div>
+    <h2>About</h2>
+    <p>Some info Bill's World.</p>
+  </div>
+
+export default AboutPage
---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router'
+import './Header.css'
+
+const Header = ({ isAuthenticated, profile, error, onLoginClick, onLogoutClick }) =>
+  <div>
+    <h1>Bill's World</h1>
+    <ul className="list-inline">
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+    </ul>
+    { !isAuthenticated ? (
+      <button onClick={onLoginClick}>Login</button>
+    ) : (
+      <div>
+        <img src={profile.picture} height="40px" />
+        <span>Welcome, {profile.nickname}</span>
+        <button onClick={onLogoutClick}>Logout</button>
+      </div>
+    )}
+    { error &&
+      <p>{error}</p>
+    }
+  </div>
+
+Header.propTypes = {
+  isAuthenticated: React.PropTypes.bool.isRequired,
+  profile: React.PropTypes.object,
+  error: React.PropTypes.string,
+  onLoginClick: React.PropTypes.func.isRequired,
+  onLogoutClick: React.PropTypes.func.isRequired
+}
+
+export default Header
