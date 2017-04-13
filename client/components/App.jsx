af6157683ef6be483b3c2b391191dab2c891d419
alex-blair <alex.n.blair@gmail.com>
2017-04-13 14:03:42 +1200

    add redux folder structure

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default React.createClass({
+class Home extends React.Component {
   render() {
     return (
       <h1>React development has begun!</h1>
     )
   }
-})
+}
+
+export default Home
