000f933a2c0ea5765806fe3b28132119c809b4f9
alex-blair <alex.n.blair@gmail.com>
2017-04-13 22:26:57 +1200

    auth-0 setup

---
+++ client/reducers/auth.js
@@ -0,0 +1,23 @@
+import AuthService from '../utils/AuthService'
+
+const initialState = {
+  isAuthenticated: !AuthService.isTokenExpired(),
+  isFetching: false,
+  profile: AuthService.getProfile(),
+  error: null
+}
+
+export default function authReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'LOGIN_REQUEST':
+      return {...state, isFetching: true, error: null}
+    case 'LOGIN_SUCCESS':
+      return {...state, isFetching: false, isAuthenticated: true, profile: action.profile}
+    case 'LOGIN_ERROR':
+      return {...state, isFetching: false, isAuthenticated: false, profile: {}, error: action.error}
+    case 'LOGOUT_SUCCESS':
+      return {...state, isAuthenticated: false, profile: {}}
+    default:
+      return state
+  }
+}
