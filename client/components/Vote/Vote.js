5bcbfa3f8e2d34025eab4dea704380ac82a80315
leo-alexander <leo.mclay@gmail.com>
2017-04-15 11:51:45 +1200

    Set up actions/reducers for votes

--- client/components/Vote/Vote.js
+++ client/components/Vote/Vote.js
@@ -1,11 +1,47 @@
 import React from 'react'
+// import Bill from '../Bill/Bill'
+// import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Vote = () => {
-  return (
-    <div>
-      <h5>Vote</h5>
-    </div>
-  )
+import { getVotes } from '../../actions/votes.js'
+
+class Vote extends React.Component {
+  componentDidMount () {
+    this.props.getVotes()
+  }
+
+  // handleClick (id) {
+  //   this.props.getBills()
+  // }
+
+  render () {
+    return (
+      <div className='votes-container'>
+        <p>Bill Number: {this.props.votes.bill_number} </p>
+        <p className='votes-for'>Votes For: {this.props.votes.votes_for} </p>
+        <p className='votes-against'>Votes Against: {this.props.votes.votes_against} </p>
+      </div>
+    )
+  }
 }
 
-export default Vote
+// Votes.propTypes = {
+//   bills: React.PropTypes.array,
+//   getBills: React.PropTypes.func
+// }
+
+const mapStateToProps = state => {
+  return {
+    votes: state.votes
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getVotes: (billNumber) => {
+      dispatch(getVotes(billNumber))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Vote)
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux'
 import test from './test'
 import auth from './auth'
 import bills from './bills'
+import votes from './votes'
 
 const reducers = combineReducers({
   test,
   auth,
-  bills
+  bills,
+  votes
 })
 
 export default reducers
