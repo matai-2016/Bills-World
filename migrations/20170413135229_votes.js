05ed77afb8ffae248c2f56f35dd5691ab69b816a
leo-alexander <leo.mclay@gmail.com>
2017-04-15 11:13:32 +1200

    modified migrations, seeds and created API route for gettting vote for singular bill

--- migrations/20170413135229_votes.js
+++ migrations/20170413135229_votes.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable('votes', table => {
   table.increments('id').primary()
   table.integer('user_id').references('users.id')
-  table.integer('bill_id').references('bills.id')
+  table.string('bill_number').references('bills.bill_number')
   table.boolean('voted_for')
   table.boolean('voted_against')
 })
--- server/routes/votes.js
+++ server/routes/votes.js
@@ -6,27 +6,21 @@ const votes = require('../lib/votes')
 const router = express.Router()
 router.use(bodyParser.json())
 
-// get single bill and vote count
-
-router.get('/:id', (req, res) => {
-  Promise.all([bill.getBill(req.params.id), votes.getVotes()])
+// returns votes for and against for singular bill
+router.get('/:bill_number', (req, res) => {
+  Promise.all([bill.getBill(req.params.bill_number), votes.getVotes()])
     .then(([bill, votes]) => {
       console.log('BILL: ', bill[0])
       const curBill = bill[0]
       const result = {
         bill_number: curBill.bill_number,
-        title: curBill.title,
-        summary: curBill.summary,
-        type: curBill.type,
-        member_in_charge: curBill.member_in_charge,
-        introduction_date: curBill.introduction_date,
         votes_for: votes
-        .filter(vote => vote.bill_id === curBill.id)
+        .filter(vote => vote.bill_number === curBill.bill_number)
         .reduce(function (total, vote) {
           return vote.voted_for ? total + 1 : total
         }, 0),
         votes_against: votes
-        .filter(vote => vote.bill_id === curBill.id)
+        .filter(vote => vote.bill_number === curBill.bill_number)
         .reduce(function (total, vote) {
           return vote.voted_against ? total + 1 : total
         }, 0)
@@ -35,16 +29,16 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.delete('/:id', (req, res) => {
-  bill.deleteBill(req.params.id)
-  .then((row) => {
-    console.log('Deleted', res.body)
-    res.send({
-      message: 'Order item deleted',
-      rowsAffected: row
-    })
-  })
-})
+// router.delete('/:id', (req, res) => {
+//   bill.deleteBill(req.params.id)
+//   .then((row) => {
+//     console.log('Deleted', res.body)
+//     res.send({
+//       message: 'Order item deleted',
+//       rowsAffected: row
+//     })
+//   })
+// })
 
 // router.post('/', (req, res) => {
 //   orderItems.createOrderItem(req.body)
