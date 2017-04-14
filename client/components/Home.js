90f2bda49a5551f02a76ed89e380b00ee355ceb9
alex-blair <alex.n.blair@gmail.com>
2017-04-14 12:04:51 +1200

    add Header component

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Bills from './Bills'
 import Login from './Login'
+import Header from './Header'
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <h1>Home Page</h1>
       <Login />
       <Bills />
--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router'
 
 import Home from '../components/Home'
+import Header from '../components/Header'
 import Login from '../components/Login'
 import Bills from '../components/Bills'
 import Bill from '../components/Bill'
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div>
         <Route exact path='/' component={Home} />
+        <Route path='/header' component={Header} />
         <Route path='/login' component={Login} />
         <Route path='/bills' component={Bills} />
         <Route path='/bill' component={Bill} />
