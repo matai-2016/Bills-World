3147caf3675aea3e921d8ebf237d89c5c70c5d7f
naeri-kailash <naerikailash@gmail.com>
2017-04-15 11:20:07 +1200

    alter bill reducer

--- client/reducers/bills.js
+++ client/reducers/bills.js
@@ -4,8 +4,6 @@ export default function bills (state = initialState, action) {
   switch (action.type) {
     case 'RECEIVE_BILLS':
       return action.bills
-    case 'RECEIVE_BILL_INFO':
-      return action.billInfo
     default:
       return state
   }
