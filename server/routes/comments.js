1d2240161009a13c62e2304dd090bb80b6e0f23c
alex-blair <alex.n.blair@gmail.com>
2017-04-15 17:31:32 +1200

    add server side route to get list of comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -5,19 +5,29 @@ const comments = require('../lib/comments')
 const router = express.Router()
 router.use(bodyParser.json())
 
+router.get('/:bill_number', (req, res) => {
+  comments.getComments(req.params.bill_number)
+      .then(function (result) {
+        res.send(result)
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err.message)
+        }
+      })
+})
 
 router.post('/save', (req, res) => {
   comments.saveComment(req.body.clientID, req.body.billNumber, req.body.comment)
       .then(() => comments.getComments(req.body.billNumber))
       .then(function (result) {
-        console.log(result)
         res.send(result)
       })
-    .catch((err) => {
-      if (err) {
-        console.error(err.message)
-      }
-    })
+      .catch((err) => {
+        if (err) {
+          console.error(err.message)
+        }
+      })
 })
 
 module.exports = router
