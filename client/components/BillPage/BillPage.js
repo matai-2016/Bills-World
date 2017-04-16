e0ea830beb7685a79e0dac03b31a265271a8cb78
naeri-kailash <naerikailash@gmail.com>
2017-04-16 17:41:14 +1200

    comment count for each bill shown on home page

--- client/components/BillPage/BillPage.js
+++ client/components/BillPage/BillPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
 import { getBillInfo } from '../../actions/billInfo'
 
@@ -16,33 +15,29 @@ class BillPage extends React.Component {
   componentDidMount () {
     const billNumber = this.props.match.params.bill_number
     this.props.getBillInfo(billNumber)
-    console.log(this.props.getBillInfo(billNumber))
   }
   render () {
     if (!this.props.billInfo.bill_number) {
       return (<div>Loading...</div>)
     }
     return (
-      <div>
+      <div className='container bill-info-container'>
         <Header />
-        <Link to='/'><img className='back' src='/img/back-button.png' /></Link>
-        <div className='container'>
-          <Login />
-          <Vote billNumber={this.props.match.params.bill_number} />
-          <BillInfo
-            title={this.props.billInfo.title}
-            introductionDate={this.props.billInfo.introduction_date}
-            memberInCharge={this.props.billInfo.member_in_charge}
-            type={this.props.billInfo.type}
-            billNumber={this.props.billInfo.bill_number}
-            summary={this.props.billInfo.summary}
+        <Login />
+        <Vote billNumber={this.props.match.params.bill_number} />
+        <BillInfo
+          title={this.props.billInfo.title}
+          introductionDate={this.props.billInfo.introduction_date}
+          memberInCharge={this.props.billInfo.member_in_charge}
+          type={this.props.billInfo.type}
+          billNumber={this.props.billInfo.bill_number}
+          summary={this.props.billInfo.summary}
+        />
+        <Discussion
+          billNumber={this.props.billInfo.bill_number}
+          comments={this.props.billInfo.comments}
+          getBillInfo={this.props.getBillInfo}
           />
-          <Discussion
-            billNumber={this.props.billInfo.bill_number}
-            comments={this.props.billInfo.comments}
-            getBillInfo={this.props.getBillInfo}
-            />
-        </div>
         <Footer />
       </div>
     )
--- client/components/CommentCount/CommentCount.js
+++ client/components/CommentCount/CommentCount.js
@@ -6,11 +6,10 @@ import './commentCount.css'
 class CommentCount extends React.Component {
   render () {
     const comments = this.props.comments
-    console.log(comments.length)
     const numberOfComments = comments.length
     return (
       <div className='container-fluid'>
-        {numberOfComments}
+        <img className='speech-bubble' src='/img/speech-bubble.jpg' /> {numberOfComments}
       </div>
     )
   }
