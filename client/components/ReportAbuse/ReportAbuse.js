613ab63d634067f721c03a0ba57de67323fa9a27
alex-blair <alex.n.blair@gmail.com>
2017-04-26 09:14:30 +1200

    fix typo in report abuse

--- client/components/ReportAbuse/ReportAbuse.js
+++ client/components/ReportAbuse/ReportAbuse.js
@@ -4,7 +4,7 @@ import './reportAbuse.css'
 const ReportAbuse = () => {
   return (
     <div className='report-abuse'>
-      <h5>If you have any questions or would like to report a another user...</h5>
+      <h5>If you have any questions or would like to report another user...</h5>
       <a className='email-link' href={'mailto:' + 'billsworldnz@gmail.com'}>Email Bill's World</a>
     </div>
   )
