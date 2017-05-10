6301cbc9bc8e9e9ee439d2b7f96ec492c14374e2
alex-blair <alex.n.blair@gmail.com>
2017-05-10 15:07:04 +1200

    add new columns to comments table

--- migrations/20170510121203_add_replies_to_comments.js
+++ migrations/20170510121203_add_replies_to_comments.js
@@ -1,11 +1,13 @@
-exports.up = knex => knex.schema.table('comments', table => {
+exports.up = function(knex, Promise) {
+  return knex.schema.table('comments', function (table) {
     table.integer('parent_id').references('comments.id')
     table.string('edited')
     table.string('deleted')
   })
 }
 
-exports.down = knex => knex.schema.table('comments', table => {
+exports.down = function(knex, Promise) {
+  return knex.schema.table('comments', function (table) {
       table.dropColumn('parent_id')
       table.dropColumn('edited')
       table.dropColumn('deleted')
